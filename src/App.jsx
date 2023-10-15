6d709aaa536c43fd13933ab9310f5ab4e8ca7757
Viache-Slav <slavikfedko@gmail.com>
2023-10-15 17:02:02 +0200

    working on the first task

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./app.css";
+import Profile from "./component/Profile/Profile";
+// import Statistics from "./Statistics/Statistics";
+// import FriendsList from "./FriendsList/FriendsList";
+// import TransactionHistory from "./TransactionsHistory/TransactionsHistory";
+import userData from "./component/Profile/user.json";
+// import statisticsDataSource from "./Statistics/data.json";
+// import friendsDataSource from "./FriendsList/friends.json";
+// import transactionDataSource from "./TransactionsHistory/transactions.json";
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      {/* <Statistics
+        statisticsData={statisticsDataSource}
+        statisticsTitle={"Upload Stats"}
+      />
+      <FriendsList friendsData={friendsDataSource} />
+      <TransactionHistory transactionData={transactionDataSource} /> */}
+    </>
+  );
+}
---
+++ src/component/Profile/Profile.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import css from "./profile.module.css";
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  const formatNumberWithCommas = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img
+          src={avatar}
+          alt="User avatar"
+          className={css.description__avatar}
+        />
+        <p className={css.description__name}>{username}</p>
+        <p className={css.description__tag}>@{tag}</p>
+        <p className={css.description__location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+      <li>
+          <span className={css.stats__label}>Followers </span>
+          <span className={css.stats__quantity}>{formatNumberWithCommas(stats.followers)}</span>
+        </li>
+        <li>
+          <span className={css.stats__label}>Views </span>
+          <span className={css.stats__quantity}>{formatNumberWithCommas(stats.views)}</span>
+        </li>
+        <li>
+          <span className={css.stats__label}>Likes </span>
+          <span className={css.stats__quantity}>{formatNumberWithCommas(stats.likes)}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Profile;
